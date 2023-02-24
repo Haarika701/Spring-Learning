63002480845a3ca525b437a5d18066c322b46a7f
Haarika Ramadugu <hramadugu@gmail.com>
2023-02-24 16:11:46 -0700

    chore:updated packagne names,for spring framework

---
+++ learn-spring-framework/src/main/java/com/in28minutes/learnspringframework/GamingConsole.java
@@ -0,0 +1,11 @@
+package com.in28minutes.learnspringframework;
+
+public interface GamingConsole {
+    void up();
+
+    void down();
+
+    void left();
+
+    void right();
+}
---
+++ learn-spring-framework/src/main/java/com/in28minutes/learnspringframework/springFramework2/App01GamingBasicJava.java
@@ -0,0 +1,8 @@
+package com.in28minutes.learnspringframework.springFramework2;
+
+public class App01GamingBasicJava {
+
+    public static void main(String[] args) {
+
+    }
+}
