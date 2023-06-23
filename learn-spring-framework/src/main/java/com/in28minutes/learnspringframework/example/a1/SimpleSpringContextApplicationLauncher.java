c45570ab81de08ab71a553b52658a16240ab3479
Haarika Ramadugu <hramadugu@gmail.com>
2023-06-23 12:42:27 -0700

    git commit -m "real world spring java application"

---
+++ learn-spring-framework/src/main/java/com/in28minutes/learnspringframework/example/a1/SimpleSpringContextApplicationLauncher.java
@@ -0,0 +1,23 @@
+package com.in28minutes.learnspringframework.example.a1;
+
+import java.util.Arrays;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+@ComponentScan
+
+public class SimpleSpringContextApplicationLauncher {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try(var context = new AnnotationConfigApplicationContext(SimpleSpringContextApplicationLauncher.class)){
+			Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
+		}
+
+	}
+
+}
